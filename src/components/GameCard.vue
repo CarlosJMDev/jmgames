84341e1583f00ae05edded330efac3e67848e68a
Carlos Jimenez <carlosjmdev@gmail.com>
2025-03-06 12:05:55 +0100

    Feat: Image optimization

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -10,17 +10,20 @@ const props = defineProps({
 <template>
   <RouterLink :to="{ name: props.link }">
     <div class="h-full flex flex-col border border-gray-700 rounded-2xl text-white">
-      <header class="m-0 rounded-t-2xl flex justify-center align-middle bg-slate-900 bg-opacity-5">
-        <img
-          :src="props.image"
-          loading="lazy"
-          :title="props.title"
-          :alt="props.title"
-          width="500"
-          height="300"
-          style="width: 100%; height: auto; border-radius: 16px 16px 0 0"
-          class="hover:scale-105 hover:rounded-b-[16px]"
-        />
+      <header class="m-0 rounded-t-2xl flex justify-center align-middle bg-opacity-5">
+        <picture class="w-[450px] h-72 aspect-7/5">
+          <source :srcset="`/games/${props.image}.avif`" type="image/avif" />
+          <source :srcset="`/games/${props.image}.webp`" type="image/webp" />
+          <img
+            :src="`/games/${props.image}.png`"
+            loading="lazy"
+            :title="props.title"
+            :alt="props.title"
+            width="450"
+            height="300"
+            class="rounded-t-[16px] hover:scale-105 hover:rounded-[16px] hover:border-l hover:border-r hover:border-t hover:border-gray-700 aspect-7/5"
+          />
+        </picture>
       </header>
       <main class="p-4 flex flex-col gap-2">
         <h1 class="text-lg flex text-[#fca311] gap-2">
