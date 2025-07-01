8bae1a98b075f370fab92c68b5b4419d8647982e
Carlos Jimenez <carlosjmdev@gmail.com>
2025-07-01 12:00:40 +0200

    Fix: Update HTML lang attribute and add meta description; Add robots.txt and sitemap.xml; Refactor PageHeader and PageFooter components

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -5,6 +5,6 @@ const i18n = inject('i18n') as { t: (key: string) => string }
 
 <template>
   <footer class="bg-[#fca311] p-3 px-8 mt-2 rounded-t-3xl flex justify-center items-center">
-    <span class="text-white font-bold">{{ i18n.t('common.footer') }}</span>
+    <span class="font-bold">{{ i18n.t('common.footer') }}</span>
   </footer>
 </template>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,7 +1,5 @@
 <template>
-  <header
-    class="absolute top-0 bg-[#fca311] w-dvw h-8 flex justify-center items-center text-white font-bold"
-  >
+  <header class="absolute top-0 bg-[#fca311] w-dvw h-8 flex justify-center items-center font-bold">
     <RouterLink :to="{ name: 'home' }">
       <h1>JMGames</h1>
     </RouterLink>
